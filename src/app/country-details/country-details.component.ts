ef1b0d898a8029dd943b03192d539e66aad7e051
Eric Duss <eric.duss@gmail.com>
2025-01-21 11:03:52 -0800

    using map.component to run debug of WorldBankServices

--- src/app/country-details/country-details.component.ts
+++ src/app/country-details/country-details.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { MapComponent } from '../map/map.component';
+import { MapService } from '../services/map.service';
+import { WorldBankService } from '../services/world-bank.service';
 
 @Component({
   selector: 'app-country-details',
   imports: [
-    
+    MapComponent,
   ],
   templateUrl: './country-details.component.html',
   styleUrl: './country-details.component.scss',
@@ -11,9 +14,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CountryDetailsComponent implements OnInit {
 
-  constructor() {}
+  constructor(private worldBankService: WorldBankService) {}
 
   ngOnInit(): void {
-    
+    this.worldBankService.loadWorldBank().subscribe({
+      next: (data) => {
+        console.log('All countries:', data);
+      },
+      error: (error) => {
+        console.log('Error:', error);
+      }
+    });
+
+    this.worldBankService.getCountryByCode('US').subscribe({
+      next: (data) => {
+        console.log('Single country:', data);
+      },
+
+      error: (error) => {
+        console.log('Error:', error);
+      }
+    });
   }
 }
