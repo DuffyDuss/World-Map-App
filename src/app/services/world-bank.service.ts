3ed7025eb28c19ba1d9f298e6734d1b8edc1ba12
Eric Duss <eric.duss@gmail.com>
2025-01-21 10:48:08 -0800

    trying to get data similar to country.ts

--- src/app/services/world-bank.service.ts
+++ src/app/services/world-bank.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { Country } from '../models/country';
 @Injectable({
   providedIn: 'root'
+
 })
 export class WorldBankService {
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
     
   public loadWorldBank() {
-    return this.http.get('https://api.worldbank.org/v2/country?format=json&per_page=300', {
+    return this.http.get('https://api.worldbank.org/v2/country/?format=json&per_page=300', {
+      responseType: 'json'
+    });
+  }
+
+  public getCountryByCode(countryCode: string) {
+    return this.http.get(`https://api.worldbank.org/v2/country/${countryCode}?format=json&per_page=300`, {
       responseType: 'json'
     });
   }
