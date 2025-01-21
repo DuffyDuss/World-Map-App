de2dd77b80c34474d370277dd41e7103e53ecd1c
Eric Duss <eric.duss@gmail.com>
2025-01-21 15:51:53 -0800

    simplify imports.
     remove getCountryByCode from earlier testing

--- src/app/country-details/country-details.component.ts
+++ src/app/country-details/country-details.component.ts
@@ -1,32 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import { MapComponent } from '../map/map.component';
-import { MapService } from '../services/map.service';
+//import { MapComponent } from '../map/map.component';
+//import { MapService } from '../services/map.service';
 import { WorldBankService } from '../services/world-bank.service';
+import { Country } from '../models/country';
 
 @Component({
   selector: 'app-country-details',
-  imports: [
-  
-  ],
+  imports: [],
   templateUrl: './country-details.component.html',
   styleUrl: './country-details.component.scss',
   standalone: true
 })
 export class CountryDetailsComponent implements OnInit {
+  countryData: Country[] = [];
 
   constructor(private worldBankService: WorldBankService) {}
 
   ngOnInit(): void {
     this.worldBankService.loadWorldBank().subscribe({
-      next: (data) => {
-        console.log('All countries:', data);
+      next: (response: any) => {
+        //console.log('All countries:', data);
+        this.countryData = this.worldBankService.mapWorldBankData(response);
+        console.log('Mapped data:', this.countryData);
       },
       error: (error) => {
         console.log('Error:', error);
       }
     });
 
-    this.worldBankService.getCountryByCode('US').subscribe({
+    /* this.worldBankService.getCountryByCode('US').subscribe({
       next: (data) => {
         console.log('Single country:', data);
       },
@@ -34,6 +36,6 @@ export class CountryDetailsComponent implements OnInit {
       error: (error) => {
         console.log('Error:', error);
       }
-    });
+    }); */
   }
 }
