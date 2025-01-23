4e2181b22db6f5db02bd5fde1e9de70d51af88e7
Eric Duss <eric.duss@gmail.com>
2025-01-23 09:16:06 -0800

    redirects request to get county code OnChanges

--- src/app/services/world-bank.service.ts
+++ src/app/services/world-bank.service.ts
@@ -15,11 +15,11 @@ export class WorldBankService {
     });
   }
 
- /* public getCountryByCode(countryCode: string) {
+ public getCountryByCode(countryCode: string) {
     return this.http.get(`https://api.worldbank.org/v2/country/${countryCode}?format=json&per_page=300`, {
       responseType: 'json'
     });
-  } */
+  } 
 
 /*  public mapWorldBankData(apiData: any[]): Country[] {
     
